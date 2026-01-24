a77efb3b6b6d6c4af7cc08bcb049eae10667aacc
DanielFFPaulo <aluno25966@ipt.pt>
2026-01-24 22:34:13 +0000

    polishing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,9 +11,9 @@ android {
     defaultConfig {
         applicationId = "pt.ipt.ddam2025.fittrack"
         minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        targetSdk = 36
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
